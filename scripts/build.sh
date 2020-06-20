01c2980fea259a9c83c37f7eafd40a2ddfa3549f
Andrey Devyatkin <andrey.devyatkin@fivexl.io>
2020-06-20 15:31:49 +0100

    restructure ad get ci to work

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,24 +2,19 @@
 
 set -ex
 
-export GO111MODULE=on
-
 BUILD_DIR="$PWD/build"
-GOPACKAGE_NAME="github.com/fivexl/ldap-radius-with-mfa"
 
 mkdir -p "$BUILD_DIR"
-cd "$BUILD_DIR"
 
 VERSION=${1}
-[ -z "${VERSION}" ] && VERSION=beta
+[ -z "${VERSION}" ] && VERSION=beta-$(date "+%Y_%m_%d_%H_%M_%S")-$(git rev-parse HEAD)
 
 TARGET_GOOSES=("linux" "darwin")
+export GOARCH=amd64
 
 for OS in ${TARGET_GOOSES[@]}
 do
   echo "Building for ${OS}"
   export GOOS=${OS}
-  export GOARCH=amd64
-  export CGO_ENABLED=0
-  go build -o "server-${GOOS}-${GOARCH}" -v -ldflags "-w -X main.VERSION=${VERSION}" "$GOPACKAGE_NAME"
+  go build -v -o "build/rserver-${GOOS}-${GOARCH}" -v -ldflags "-w -X main.VERSION=${VERSION}"
 done
